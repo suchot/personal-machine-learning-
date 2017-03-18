9b04963024cb0982802e4e842edf435f16352f37
hubin <403620968@qq.com>
2017-03-18 22:43:19 +0800

    upto handwriteing learning“

---
+++ kNN.py
@@ -0,0 +1,79 @@
+from numpy import *
+import operator
+
+def createDataSet():
+    group = array ([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**2
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)# 源代码中iteritems修改为items 
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOLines =fr.readlines()
+    numberOfLines = len(arrayOLines)     # get the number of lines return
+    returnMat = zeros((numberOfLines, 3))# prepare matrix to return
+    classLabelVector = []                # prepare label return
+    index = 0
+    for line in arrayOLines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+
+def autoNorm(dataSet):
+    minVals = dataSet.min(0)
+    maxVals = dataSet.max(0)
+    ranges = maxVals -minVals
+    normDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normDataSet = dataSet - tile(minVals, (m,1))# 利用NumPy库中的tile()将变量内容复制成输入矩阵同样大小的矩阵
+    normDataSet = normDataSet/tile(ranges, (m,1))# 此处为具体特征值相除。
+    return normDataSet, ranges, minVals
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt') 
+    normMat, ranges,minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]#shape函数可以导出矩阵的形状[0]为行数[1]为列数
+    numTestVecs = int(m*hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],5)
+        print ("the classifier came back with: %d,the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount/float(numTestVecs)))
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per years?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges, normMat, datingLabels, 3)
+    print("you will probably like this person:", resultList[classifierResult - 1])
+
+def img2vector(filename):
+    returnVect = zeros((1,1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+        return returnVect
+        
