ff8d04d5bd903acaeed03771b7b540d7ce919a08
hubin <403620968@qq.com>
2017-03-19 16:40:25 +0800

    fixed bugs and code handwritingClassTest

--- kNN.py
+++ kNN.py
@@ -1,16 +1,17 @@
 from numpy import *
 import operator
-
+from os import listdir
 def createDataSet():
     group = array ([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group, labels
+	
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances**2
+    distances = sqDistances**0.5 #应该为0.5
     sortedDistIndicies = distances.argsort()
     classCount={}
     for i in range(k):
@@ -75,5 +76,29 @@ def img2vector(filename):
         lineStr = fr.readline()
         for j in range(32):
             returnVect[0,32*i+j] = int(lineStr[j])
-        return returnVect
-        
+    return returnVect
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = listdir('G:\\桌面\\jupyter notebook\\digits\\trainingDigits')           #load the training set
+    m = len(trainingFileList)
+    trainingMat = zeros((m,1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i,:] = img2vector('G:\\桌面\\jupyter notebook\\digits\\trainingDigits\\%s' % fileNameStr)
+    testFileList = listdir('G:\\桌面\\jupyter notebook\\digits\\testDigits')        #iterate through the test set
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('G:\\桌面\\jupyter notebook\\digits\\testDigits\\%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr): errorCount += 1.0
+    print ("\nthe total number of errors is: %d" % errorCount)
+    print ("\nthe total error rate is: %f" % (errorCount/float(mTest)))
